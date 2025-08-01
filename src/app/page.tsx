02e5c41e8d79972f623f046d84acb3c5cdd5cd90
unknown <tranminhlong2404@gmail.com>
2025-08-01 14:17:53 +0700

    feat: custom Home page with VLU Layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,13 @@ import Layout from '../components/Layout';
 export default function Home() {
   return (
     <Layout>
-      <h1 className="text-2xl font-bold">
+      <h1 className="text-2xl font-bold mb-6">
         Trang Trung tâm Hỗ trợ Sinh viên VLU
       </h1>
-      {/* TODO: nội dung chính */}
+      {/* TODO: Thêm các section nội dung ở đây */}
+      <p className="text-base mb-4">
+        Chào mừng bạn đến với chatbot hỗ trợ sinh viên VLU. Hãy bấm vào icon chat để bắt đầu hỏi!
+      </p>
     </Layout>
   );
 }
